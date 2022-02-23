99d978b0f36fa8c9d954c9b8a1bfa88c1c318b36
John <dignosjohn@yahoo.com>
2022-02-23 19:26:37 +0800

    CHANGED

--- script.js
+++ script.js
@@ -1,105 +1,90 @@
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll("button");
+const choices = ["rock", "paper", "scissors"];
+const resultRef = document.getElementById("result");
 
-
-let userChoice;
+let movesLeft = document.getElementById("movesLeft");
+let userCHoice;
 let computerChoice;
 
-let moves = 0;
 let userScore = 0;
 let computerScore = 0;
 let drawScore = 0;
-
-
-let resultRef = document.getElementById('result')
+let moves = 0;
 
 const handleClick = (e) => {
-     userChoice = e.target.id
-    document.getElementById('user_choice').innerText = `You Choose ${userChoice.toUpperCase()}`;
-    document.getElementById('user_score').innerText = userScore;
-    const movesLeft = document.getElementById('movesLeft');
-    moves++
-    movesLeft.innerText = `${10 - moves}`;
-     
-    generateRandomCompChoices()
-    getResult()
+  userCHoice = e.target.id;
+  document.getElementById(
+    "user_choice"
+  ).innerText = `You Choose ${userCHoice.toUpperCase()}`;
+  document.getElementById("user_score").innerText = `${userScore}`;
+  document.getElementById("draws").innerText = `${drawScore}`;
+  ++moves;
+  movesLeft.innerText = `${10 - moves}`;
+  computerC();
+  winner();
+  if (moves == 10) {
+    gameOver(buttons, movesLeft);
+  }
+};
 
-    if(moves == 10){gameOver(buttons,movesLeft)}
+const computerC = () => {
+  computerChoice = choices[Math.floor(Math.random() * 3)];
+  document.getElementById(
+    "comp_choice"
+  ).textContent = `Computer Choose ${computerChoice.toUpperCase()}`;
+  document.getElementById("computer_score").innerText = `${computerScore}`;
+};
+
+const winner = () => {
+  switch (userCHoice + computerChoice) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      ++userScore;
+      resultRef.style.cssText = "background-color: #cefdce; color: #689f38";
+      resultRef.textContent = `YOU WIN`;
+      break;
+    case "rockspaper":
+    case "paperscissors":
+    case "scissorsrock":
+      ++computerScore;
+      resultRef.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+      resultRef.textContent = `YOU LOSE`;
+      break;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      ++drawScore;
+      resultRef.style.cssText = "background-color: #e5e5e5; color: #808080";
+      resultRef.textContent = `DRAW`;
+  }
+};
+
+const gameOver = (buttons, movesLeft) => {
+  buttons.forEach((buttons) => {
+    buttons.style.display = "none";
+  });
+
+  const restartBtn = document.getElementById("restart");
+  if (userScore > computerScore) {
+    resultRef.style.cssText = "background-color: #cefdce; color: #689f38";
+    resultRef.innerText = "YOU WON THE GAME";
+  } else if (userScore < computerScore) {
+    resultRef.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+    resultRef.innerText = "YOU LOSE THE GAME";
+  } else if (userScore === computerScore) {
+    resultRef.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+    resultRef.innerText = "YOU LOSE THE GAME";
+  }
+  restartBtn.innerText = "RESTART GAME?";
+  restartBtn.style.display = "flex";
+  restartBtn.style.cssText = "background-color: #ffd51b;";
+  restartBtn.addEventListener("click", () => {
+    window.location.reload();
+  });
+};
+
+function game() {
+  buttons.forEach((buttons) => buttons.addEventListener("click", handleClick));
 }
-
-const generateRandomCompChoices = () =>{
-    const choices = ['rock','paper','scissors']
-    const randomNum = Math.floor(Math.random() * 3)
-    computerChoice = choices[randomNum]
-    document.getElementById('comp_choice').innerText = `Computer Choose ${computerChoice.toUpperCase()}`;
-    document.getElementById('computer_score').innerText = computerScore;
-}
-
-const getResult = () => { 
-     document.getElementById('draws').innerText = drawScore;
-
-     switch(userChoice + computerChoice){
-          case 'rockscissors':
-          case 'scissorspaper':
-          case 'paperrock':
-               ++userScore
-               resultRef.style.cssText = 
-               "background-color: #cefdce; color: #689f38";
-               resultRef.innerText = `YOU WIN`
-          break;
-          case 'rockpaper':
-          case 'paperscissors':
-          case 'scissorsrock':
-               ++computerScore
-               resultRef.style.cssText = 
-               "background-color: #ffdde0; color: #d32f2f"
-               resultRef.innerText =`YOU LOSE`
-          break;
-          case 'rockrock':
-          case 'paperpaper':
-          case 'scissorsscissors':
-               ++drawScore
-               resultRef.style.cssText = 
-               "background-color: #e5e5e5; color: #808080";
-               resultRef.innerText = `DRAW`
-     }
-}
-
-function gameOver(buttons,movesLeft){
-buttons.forEach(buttons => {
-     buttons.style.display = 'none'
-})
-
-resultRef.innerText = 'GAME OVER!!'
-movesLeft.style.display = 'none';
-const restartBtn = document.getElementById('restart')
-
-if(userScore > computerScore){
-     resultRef.style.cssText = 
-     "background-color: #cefdce; color: #689f38";
-     resultRef.innerText = "YOU WON THE GAME";
-
-}else if(computerScore > userScore){
-     resultRef.style.cssText = 
-     "background-color: #ffdde0; color: #d32f2f"
-     resultRef.innerText = "YOU LOSE THE GAME"
-}else if(userScore === computerScore){
-     resultRef.style.cssText = 
-     "background-color: #e5e5e5; color: #808080"
-     resultRef.innerText = "DRAW GAME"
-}
-
-restartBtn.innerText = 'RESTART GAME?';
-restartBtn.style.display = 'flex';
-restartBtn.style.cssText ="background-color: #ffd51b;"
-restartBtn.addEventListener('click',() => {
-     window.location.reload();
-})
-}
-
-
-function game(){
-buttons.forEach(buttons => buttons.addEventListener('click',handleClick))
-}
-
-game()
-
+game();
